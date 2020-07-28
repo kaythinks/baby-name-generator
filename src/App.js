ddf51728e4db52859e07e5c1a36138e3bcc565de
Ezekiel <iamwebwiz@gmail.com>
2020-07-28 01:22:53 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-import Names from './components/names';
+import React, { Component } from "react";
+import Names from "./components/names";
+import { generateName } from "./utils/nameGenerator";
 
 class App extends Component {
-
-state = { boys : [ 'kelvin','marley','seun','kanye','john','paul'] ,
-  girls : ['bola','ayo','ola','rebecca','rachael','elizabeth']
-}
-
-generateName = (name) => {
-  if (name === 'boy') alert('Your baby name is :- ' + this.state.boys[Math.round(Math.random() * (this.state.boys).length )]  + ', congrats !!! ' )
-  if (name === 'girl') alert('Your baby name is :- ' + this.state.girls[Math.round(Math.random() * (this.state.girls).length )]  + ', congrats !!! ')  
-}  
-
-render() {
-  return (
-    <div>
-      <Names generateName={this.generateName} />
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <Names generateName={generateName} />
+      </div>
+    );
+  }
 }
 
 export default App;
